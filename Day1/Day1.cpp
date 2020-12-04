7275a24aaf5487b0e88a77a2d33bfb61c80a04cc
Julio Ernesto Rodríguez Cabañas <julioernesto90@gmail.com>
2020-12-04 14:10:24 +0000

    Implemented solutions for the first two puzzles

---
+++ Day1/Day1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	list<int> numbers;
+
+	while (!cin.eof())
+	{
+		int number;
+		cin >> number;
+
+		for (auto const& previous_number : numbers)
+		{
+			if (previous_number + number == 2020)
+			{
+				cout << previous_number * number;
+				return 0;
+			}
+		}
+
+		numbers.push_back(number);
+	}
+
+	return -1;
+}
---
+++ Day2/Day2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int validTotal = 0;
+
+	while (!cin.eof())
+	{
+		int min;
+		int max;
+		char character;
+		string password;
+		char _;
+		cin >> min >> _ >> max >> character >> _ >> password;
+
+		int occurrences = 0;
+		for (int i = 0; i < password.size(); i++)
+		{
+			char password_char = password.at(i);
+			occurrences += character == password_char ? 1 : 0;
+		}
+
+		if (occurrences >= min && occurrences <= max)
+			validTotal++;
+	}
+
+	cout << validTotal;
+
+	return 0;
+}
