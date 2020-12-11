06c5171c759e83d759a82c0636145dab2e09b5e3
Julio Ernesto Rodríguez Cabañas <julioernesto90@gmail.com>
2020-12-11 16:54:07 +0000

    Cleaned up some code

--- Day11/day11.cpp
+++ Day11/day11.cpp
@@ -2,58 +2,55 @@
 
 int main()
 {
-	//int result = part1();
-	int result = part2();
-	cout << result;
-
+	const auto layout = read_layout();
+	std::cout << "Part 1: " << part1(layout) << std::endl;
+	std::cout << "Part 2: " << part2(layout) << std::endl;
 	return 0;
 }
 
-int part1()
+int part1(std::vector<std::string> layout)
 {
-	vector<string> layout = read_layout();
 	return count_occupied_seats_when_movement_stops(layout, 4, false);
 }
 
-int part2()
+int part2(std::vector<std::string> layout)
 {
-	vector<string> layout = read_layout();
 	return count_occupied_seats_when_movement_stops(layout, 5, true);
 }
 
-vector<string> read_layout()
+std::vector<std::string> read_layout()
 {
-	vector<string> layout;
-	for (string line; getline(cin, line); layout.push_back(line));
+	std::vector<std::string> layout;
+	for (std::string line; std::getline(std::cin, line); layout.push_back(line));
 	return layout;
 }
 
-int count_occupied_seats_when_movement_stops(vector<string> layout, int tolerance, bool look_further)
+int count_occupied_seats_when_movement_stops(std::vector<std::string> layout, int tolerance, bool look_further)
 {
-	vector<string> next_layout;
+	std::vector<std::string> next_layout;
 	while (!are_layouts_equal(&layout, &(next_layout = calculate_next_layout(&layout, tolerance, look_further))))
 		layout = next_layout;
 
-	int total_occupied = 0;
-	for (int i = 0; i < layout.size(); i++)
-		for (int j = 0; j < layout[i].size(); j++)
+	auto total_occupied = 0;
+	for (std::size_t i = 0; i < layout.size(); i++)
+		for (std::size_t j = 0; j < layout[i].size(); j++)
 			if (layout[i][j] == '#') total_occupied++;
 
 	return total_occupied;
 }
 
-vector<string> calculate_next_layout(vector<string>* layout_ptr, int tolerance, bool look_further)
+std::vector<std::string> calculate_next_layout(std::vector<std::string>* layout_ptr, int tolerance, bool look_further)
 {
-	vector<string> next_layout;
-	for (int i = 0; i < layout_ptr->size(); i++)
+	std::vector<std::string> next_layout;
+	for (std::size_t i = 0; i < layout_ptr->size(); i++)
 	{
-		string next_row = (*layout_ptr)[i];
-		for (int j = 0; j < (*layout_ptr)[i].size(); j++)
+		std::string next_row = (*layout_ptr)[i];
+		for (std::size_t j = 0; j < next_row.size(); j++)
 		{
-			char character = (*layout_ptr)[i][j];
+			auto character = next_row[j];
 			if (character != '.')
 			{
-				int occupied_adjacent = count_occupied_adjacent(layout_ptr, i, j, look_further);
+				const auto occupied_adjacent = count_occupied_adjacent(layout_ptr, i, j, look_further);
 				if (character == 'L' && occupied_adjacent == 0) character = '#'; // Empty seat with no adjacent occupied seats becomes occupied
 				if (character == '#' && occupied_adjacent >= tolerance) character = 'L'; // Occupied seat with <tolerance> or more adjacent occupied seats becomes empty
 				next_row[j] = character;
@@ -65,23 +62,23 @@ vector<string> calculate_next_layout(vector<string>* layout_ptr, int tolerance, 
 	return next_layout;
 }
 
-int count_occupied_adjacent(vector<string>* layout_ptr, int i, int j, bool look_further)
+int count_occupied_adjacent(std::vector<std::string>* layout_ptr, std::size_t i, std::size_t j, bool look_further)
 {
-	return (is_visible_seat_occupied(layout_ptr, i, j, -1, -1, look_further) ? 1 : 0) +
-		(is_visible_seat_occupied(layout_ptr, i, j, -1, 0, look_further) ? 1 : 0) +
-		(is_visible_seat_occupied(layout_ptr, i, j, -1, +1, look_further) ? 1 : 0) +
-		(is_visible_seat_occupied(layout_ptr, i, j, 0, -1, look_further) ? 1 : 0) +
-		(is_visible_seat_occupied(layout_ptr, i, j, 0, +1, look_further) ? 1 : 0) +
-		(is_visible_seat_occupied(layout_ptr, i, j, +1, -1, look_further) ? 1 : 0) +
-		(is_visible_seat_occupied(layout_ptr, i, j, +1, 0, look_further) ? 1 : 0) +
-		(is_visible_seat_occupied(layout_ptr, i, j, +1, +1, look_further) ? 1 : 0);
+	const std::pair<int, int> adjacent_directions[8] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1} };
+
+	auto occupied_adyacent_count = 0;
+	for (const auto direction : adjacent_directions)
+		if (is_visible_seat_occupied(layout_ptr, i, j, direction.first, direction.second, look_further))
+			occupied_adyacent_count++;
+
+	return occupied_adyacent_count;
 }
 
-bool is_visible_seat_occupied(vector<string>* layout_ptr, int i, int j, int i_dir, int j_dir, bool look_further)
+bool is_visible_seat_occupied(std::vector<std::string>* layout_ptr, std::size_t i, std::size_t j, int i_dir, int j_dir, bool look_further)
 {
-	int limit_i = i_dir > 0 ? layout_ptr->size() : -1;
-	int limit_j = j_dir > 0 ? (*layout_ptr)[0].size() : -1;
-	for (int next_i = i + i_dir, next_j = j + j_dir; next_i != limit_i && next_j != limit_j; next_i += i_dir, next_j += j_dir)
+	const std::size_t limit_i = i_dir > 0 ? layout_ptr->size() : -1;
+	const std::size_t limit_j = j_dir > 0 ? (*layout_ptr)[0].size() : -1;
+	for (std::size_t next_i = i + i_dir, next_j = j + j_dir; next_i != limit_i && next_j != limit_j; next_i += i_dir, next_j += j_dir)
 	{
 		if ((*layout_ptr)[next_i][next_j] == '.')
 		{
@@ -93,10 +90,10 @@ bool is_visible_seat_occupied(vector<string>* layout_ptr, int i, int j, int i_di
 	return false;
 }
 
-bool are_layouts_equal(vector<string>* layout_ptr, vector<string>* other_layout_ptr)
+bool are_layouts_equal(std::vector<std::string>* layout_ptr, std::vector<std::string>* other_layout_ptr)
 {
-	for (int i = 0; i < layout_ptr->size(); i++)
-		for (int j = 0; j < (*layout_ptr)[i].size(); j++)
+	for (std::size_t i = 0; i < layout_ptr->size(); i++)
+		for (std::size_t j = 0; j < (*layout_ptr)[i].size(); j++)
 			if ((*layout_ptr)[i][j] != (*other_layout_ptr)[i][j])
 				return false;
 
--- Day11/day11.h
+++ Day11/day11.h
@@ -1,15 +1,13 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <utility>
 
-using namespace std;
-
-int part1();
-int part2();
-
-vector<string> read_layout();
-int count_occupied_seats_when_movement_stops(vector<string> layout, int tolerance = 4, bool look_further = false);
-vector<string> calculate_next_layout(vector<string>* layout_ptr, int tolerance, bool look_further);
-int count_occupied_adjacent(vector<string>* layout_ptr, int i, int j, bool look_further);
-bool is_visible_seat_occupied(vector<string>* layout_ptr, int i, int j, int i_dir, int j_dir, bool look_further);
-bool are_layouts_equal(vector<string>* layout_ptr, vector<string>* other_layout_ptr);
+int part1(std::vector<std::string> layout);
+int part2(std::vector<std::string> layout);
+std::vector<std::string> read_layout();
+int count_occupied_seats_when_movement_stops(std::vector<std::string> layout, int tolerance = 4, bool look_further = false);
+std::vector<std::string> calculate_next_layout(std::vector<std::string>* layout_ptr, int tolerance, bool look_further);
+int count_occupied_adjacent(std::vector<std::string>* layout_ptr, std::size_t i, std::size_t j, bool look_further);
+bool is_visible_seat_occupied(std::vector<std::string>* layout_ptr, std::size_t i, std::size_t j, int i_dir, int j_dir, bool look_further);
+bool are_layouts_equal(std::vector<std::string>* layout_ptr, std::vector<std::string>* other_layout_ptr);
