190f5aabab636e1bae21ce457166ddcd8d29b550
Julio Ernesto Rodríguez Cabañas <julioernesto90@gmail.com>
2020-12-14 18:56:35 +0000

    Cleaned up the code

--- Day14/day14.cpp
+++ Day14/day14.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-	auto start_time = std::chrono::steady_clock::now();
+	const auto start_time = std::chrono::steady_clock::now();
 
-	//auto result = part1();
-	auto result = part2();
-	std::cout << "Result: " << result << std::endl;
+	//const auto total = part1();
+	const auto total = part2();
+	std::cout << "Result: " << total << std::endl;
 
-	auto end_time = std::chrono::steady_clock::now();
+	const auto end_time = std::chrono::steady_clock::now();
 	const std::chrono::duration<double> elapsed_seconds = end_time - start_time;
 	std::cout << "Elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
 
@@ -19,8 +19,7 @@ unsigned long long part1()
 {
 	const auto memory = read_memory(false);
 	unsigned long long total = 0;
-	for (auto memory_value : memory) total += memory_value.second;
-
+	for (const auto memory_value : memory) total += memory_value.second;
 	return total;
 }
 
@@ -28,18 +27,17 @@ unsigned long long part2()
 {
 	const auto memory = read_memory(true);
 	unsigned long long total = 0;
-	for (auto memory_value : memory) total += memory_value.second;
-
+	for (const auto memory_value : memory) total += memory_value.second;
 	return total;
 }
 
 std::map<unsigned long long, unsigned long long> read_memory(bool use_address_decoder)
 {
 	std::map<unsigned long long, unsigned long long> memory;
-	unsigned long long or = 0, and = 0b111111111111111111111111111111111111;
+	unsigned long long or_mask = 0, and_mask = 0b111111111111111111111111111111111111;
+	std::string mask;
 
 	char _[6];
-	std::string mask;
 	char command;
 	std::cin.get();
 	while (!std::cin.eof() && (command = std::cin.get()))
@@ -48,24 +46,16 @@ std::map<unsigned long long, unsigned long long> read_memory(bool use_address_de
 		{
 			std::cin.read(_, 5);
 			std::cin >> mask;
-			or = 0;
-			and = 0b111111111111111111111111111111111111;
 
-			for (std::size_t i = 0; i < mask.size(); i++)
+			if (!use_address_decoder)
 			{
-				const char mask_char = mask[i];
-				if (mask_char != 'X')
-				{
-					unsigned short bit;
-					std::stringstream mask_helper;
-					mask_helper << mask_char;
-					mask_helper >> bit;
-
-					if (bit == 1)
-						or += std::pow(2, mask.size() - i - 1);
-					else
-						and -= std::pow(2, mask.size() - i - 1);
-				}
+				or_mask = 0;
+				and_mask = 0b111111111111111111111111111111111111;
+				for (std::size_t i = 0; i < mask.size(); i++)
+					if (mask[i] == '1')
+						or_mask += std::pow(2, mask.size() - i - 1);
+					else if (mask[i] == '0')
+						and_mask -= std::pow(2, mask.size() - i - 1);
 			}
 		}
 		else if (command == 'e') // Memory access
@@ -75,29 +65,16 @@ std::map<unsigned long long, unsigned long long> read_memory(bool use_address_de
 			std::cin >> address;
 			std::cin.read(_, 4);
 			std::cin >> value;
+			value = value & and_mask | or_mask;
 
-			if (!use_address_decoder)
+			const auto memory_addresses = !use_address_decoder ? std::vector<unsigned long long> { address } : get_decoded_addresses(address, mask, 0);
+			for (const auto memory_address : memory_addresses)
 			{
-				value |= or;
-				value &= and;
-
 				std::map<unsigned long long, unsigned long long>::iterator it;
-				if ((it = memory.find(address)) == memory.end())
-					memory.insert(std::make_pair(address, value));
+				if ((it = memory.find(memory_address)) == memory.end())
+					memory.insert(std::make_pair(memory_address, value));
 				else
 					it->second = value;
-			}
-			else
-			{
-				std::vector<unsigned long long> addresses = get_decoded_addresses(address, mask, 0);
-				for (const auto decoded_address : addresses)
-				{
-					std::map<unsigned long long, unsigned long long>::iterator it;
-					if ((it = memory.find(decoded_address)) == memory.end())
-						memory.insert(std::make_pair(decoded_address, value));
-					else
-						it->second = value;
-				}
 			}
 		}
 
@@ -110,48 +87,34 @@ std::map<unsigned long long, unsigned long long> read_memory(bool use_address_de
 
 std::vector<unsigned long long> get_decoded_addresses(unsigned long long address, std::string mask, std::size_t index)
 {
+	const char mandatory_zero_char = '#'; // Normal zero characters are ignored here, but this special representation of a zero will be applied
+	const char floating_bit_char = 'X';
 	std::vector<unsigned long long> result;
-
-	if (index == mask.size())
+	
+	if (index == mask.size()) // Base case: no more characters to process, so now we apply the final mask(s) to the address
 	{
-		unsigned long long or = 0, and = 0b111111111111111111111111111111111111;
+		unsigned long long or_mask = 0, and_mask = 0b111111111111111111111111111111111111;
 		for (std::size_t i = 0; i < mask.size(); i++)
+			if (mask[i] != mandatory_zero_char)
+				or_mask += mask[i] == '1' ? std::pow(2, mask.size() - i - 1) : 0;
+			else
+				and_mask -= std::pow(2, mask.size() - i - 1);
+
+		result.push_back(address & and_mask | or_mask);
+	}
+	else if (mask[index] == floating_bit_char) // Floating bit: we need to replace it with a zero AND with a one
+	{
+		for (auto const mask_char : { mandatory_zero_char, '1' })
 		{
-			if (mask[i] != '.' && mask[i] != '_')
-			{
-				unsigned short bit;
-				std::stringstream mask_helper;
-				mask_helper << mask[i];
-				mask_helper >> bit;
-				or += bit == 1 ? std::pow(2, mask.size() - i - 1) : 0;
-			}
-			else if (mask[i] == '.') // Mandatory 0
-			{
-				and -= std::pow(2, mask.size() - i - 1);
-			}
-			else if (mask[i] == '_') // Mandatory 1
-			{
-				or += std::pow(2, mask.size() - i - 1);
-			}
+			mask[index] = mask_char;
+			const auto mask_addresses = get_decoded_addresses(address, mask, index + 1);
+			for (const auto address_to_add : mask_addresses)
+				result.push_back(address_to_add);
 		}
-		result.push_back(address & and| or);
 	}
-	else if (mask[index] == 'X')
+	else // No processing needed here: we just get the result from the next level
 	{
-		auto mask1 = mask;
-		auto mask2 = mask;
-		mask1[index] = '.'; // Mandatory 0
-		mask2[index] = '_'; // Mandatory 1
-		auto mask1_addresses = get_decoded_addresses(address, mask1, index + 1);
-		auto mask2_addresses = get_decoded_addresses(address, mask2, index + 1);
-		for (const auto address_to_add : mask1_addresses)
-			result.push_back(address_to_add);
-		for (const auto address_to_add : mask2_addresses)
-			result.push_back(address_to_add);
-	}
-	else
-	{
-		return get_decoded_addresses(address, mask, index + 1);
+		result = get_decoded_addresses(address, mask, index + 1);
 	}
 
 	return result;
