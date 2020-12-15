720ce1ba3a3077a4be5c1ac09aedcaa1a47208e8
Julio Ernesto Rodríguez Cabañas <julioernesto90@gmail.com>
2020-12-15 17:55:57 +0000

    Improved algorithm

--- Day15/day15.cpp
+++ Day15/day15.cpp
@@ -4,9 +4,11 @@ int main()
 {
 	const auto start_time = std::chrono::steady_clock::now();
 
-	//const auto number = part1();
-	const auto number = part2();
-	std::cout << "Result: " << number << std::endl;
+	const auto numbers = read_numbers();
+	const auto result1 = part1(numbers);
+	std::cout << "Part 1: " << result1 << std::endl;
+	const auto result2 = part2(numbers);
+	std::cout << "Part 2: " << result2 << std::endl;
 
 	const auto end_time = std::chrono::steady_clock::now();
 	const std::chrono::duration<double> elapsed_seconds = end_time - start_time;
@@ -15,44 +17,47 @@ int main()
 	return 0;
 }
 
-unsigned int part1()
+unsigned int part1(std::vector<unsigned int> numbers)
 {
-	return read_numbers_until_iteration(2020);
+	return calculate_number(numbers, 2020);
 }
 
-unsigned int part2()
+unsigned int part2(std::vector<unsigned int> numbers)
 {
-	return read_numbers_until_iteration(30000000);
+	return calculate_number(numbers, 30000000);
 }
 
-unsigned int read_numbers_until_iteration(std::size_t last_iteration)
+std::vector<unsigned int> read_numbers()
 {
-	unsigned int number = 0;
-	std::unordered_map<unsigned int, std::vector<std::size_t>> numbers_with_index;
-	std::unordered_map<unsigned int, std::vector<std::size_t>>::iterator numbers_map_iterator;
-	std::size_t iteration = 0;
-	while (iteration++ < last_iteration)
+	std::vector<unsigned int> numbers;
+
+	while (!std::cin.eof())
 	{
-		if (!std::cin.eof())
-			std::cin >> number;
-		else
-			number = ((numbers_map_iterator = numbers_with_index.find(number)) != numbers_with_index.end() && numbers_map_iterator->second.size() > 1)
-			? numbers_map_iterator->second[numbers_map_iterator->second.size() - 1] - numbers_map_iterator->second[numbers_map_iterator->second.size() - 2]
-			: 0;
+		int number;
+		std::cin >> number;
+		numbers.push_back(number);
 
-		if ((numbers_map_iterator = numbers_with_index.find(number)) == numbers_with_index.end())
-		{
-			numbers_with_index.insert(std::make_pair(number, std::vector<std::size_t> { iteration }));
-		}
-		else
-		{
-			numbers_map_iterator->second.push_back(iteration);
-			if (numbers_map_iterator->second.size() == 3)
-				numbers_map_iterator->second.erase(numbers_map_iterator->second.begin());
-		}
-
-		if (!std::cin.eof() && !std::isdigit(std::cin.peek()))
+		while (!std::cin.eof() && !std::isdigit(std::cin.peek()))
 			std::cin.get();
+	}
+
+	return numbers;
+}
+
+unsigned int calculate_number(std::vector<unsigned int> numbers, std::size_t last_iteration)
+{
+	std::unordered_map<unsigned int, std::size_t> numbers_with_last_index;
+	std::size_t iteration;
+	for (iteration = 0; iteration < (numbers.size() - 1); iteration++)
+		numbers_with_last_index[numbers[iteration]] = iteration;
+
+	unsigned int number = 0, previous_number = numbers[iteration];
+	while (++iteration < last_iteration)
+	{
+		const auto it = numbers_with_last_index.find(previous_number);
+		number = it != numbers_with_last_index.end() ? (iteration - 1 - it->second) : 0;
+		numbers_with_last_index[previous_number] = iteration - 1;
+		previous_number = number;
 	}
 
 	return number;
--- Day15/day15.h
+++ Day15/day15.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <chrono>
 
-unsigned int part1();
-unsigned int part2();
-unsigned int read_numbers_until_iteration(std::size_t last_iteration);
+unsigned int part1(std::vector<unsigned int> numbers);
+unsigned int part2(std::vector<unsigned int> numbers);
+std::vector<unsigned int> read_numbers();
+unsigned int calculate_number(std::vector<unsigned int> numbers, std::size_t last_iteration);
